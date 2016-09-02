837f2b4a4fcdb8b1aafa775d475e005cc5c4bd36
Corwin Crownover <corwin.crownover@gmail.com>
2016-09-02 11:57:16 -0700

    Add body-parser, morgan middleware

--- poke-mongodb/server/resources/pokemon/pokemonRouter.js
+++ poke-mongodb/server/resources/pokemon/pokemonRouter.js
@@ -1,9 +1,24 @@
 var pokemonRouter = require('express').Router();
 var pokemonController = require('./pokemonController');
 
+///// QUESTIONS //////
+// what is Router .route format?
+// what is :number format?
+
+
 // Create route handlers for each of the six methods in pokemonController
 pokemonRouter.route('/')
 
 pokemonRouter.route('/:number')
 
+
 module.exports = pokemonRouter;
+
+
+
+
+
+
+// TODO
+// Create a router in `resources/pokemon/pokemonRouter.js` that utilizes 
+//each of your controller's methods. Be sure to handle errors appropriately.
--- poke-mongodb/server/server.js
+++ poke-mongodb/server/server.js
@@ -2,14 +2,19 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var morgan = require('morgan');
 var db = require('./db');
+var pokemonRouter = require('./pokemon/pokemonRouter.js');
 
 // Create the Express application:
 var app = express();
 
 // Attach middleware:
-
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(morgan('dev'));
 
 // Import the pokemonRouter and assign it to the correct route:
+// TODO: CHECK path name
+app.use('/pokemonRouter', pokemonRouter);
 
 
 app.get('/', function (req, res) {
