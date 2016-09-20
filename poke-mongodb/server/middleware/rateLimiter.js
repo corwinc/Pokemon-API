b5c58a918c3c84830e84c53b21d93f8f0efda220
Corwin Crownover <corwin.crownover@gmail.com>
2016-09-19 17:30:18 -0700

    Complete rate limiter
    - Add functionality for checking elapsed time

--- poke-mongodb/server/middleware/rateLimiter.js
+++ poke-mongodb/server/middleware/rateLimiter.js
@@ -1,65 +1,46 @@
+// NOTE: Currently using a hardcoded dummy user. Ideally, this would get a user id via authentication if it were set up.
+// NOTE: On '/' load, there are 2 get requests sent. Didn't take the time to debug it, but it affects the rateLimiting (counts 2x);
+
+var storage = {};
+
 var rateLimiter = function (req, res, next) {
-  // console.log('INSIDE RATE LIMITER! req.url & user:', req.url, req.headers.user);
-  var storage = {};
   var user = req.headers.user || undefined;
 
+  // IF REQUEST HAS USER DEFINED
   if (req.url === '/api/pokemon' && user !== undefined) {
-    // console.log('user found!');
-    if (!storage.user) {
-      console.log('there\'s no storage.user!');
-      storage[user] = 1;
-      console.log('storage:', storage);
+    // IF NEW USER & NEW USER SESSION
+    if (!storage[user]) {
+      storage[user] = {};
+      storage[user].requests = 1;
+
+      var date = new Date();
+      console.log('date', date);
+      storage[user].startTime = date;
+      console.log('new user rateLimiter:', storage[user]);
     }
 
-    if (storage[user] < 100) {
-      console.log('storage[user] < 100');
-      storage[user]++;
-      console.log('storage++:', storage);       
+    // CHECK REQUEST COUNT & TIME
+    var currentTime = Date.now();
+    var elapsedTimeS = (currentTime - storage[user].startTime) / 1000; // seconds
+    // console.log('elapsed time seconds:', elapsedTimeS);
+    if (elapsedTimeS < 3600) {
+      if (storage[user].requests < 100) {
+        storage[user].requests++;
+      } else {
+        res.status(500).send('You\'ve hit your limit! Come back in an hour.');
+      }
+    // if it's been more than 1 hour, reset start time and request counter
     } else {
-      res.status(500).send('You\'re over the limit. Please wait an hour before sending another request.');
+      storage[user].startTime = currentTime;
+      storage[user].requests = 1;
     }
-    
+
+  // IF NO USER SENT IN HEADERS
   } else if (req.url === '/api/pokemon' && req.headers.user === undefined) {
-    res.status(500).send('No user is specified.');
+    res.status(500).send('No user specified in request');
   }
 
   next();
 };
 
 module.exports = rateLimiter;
-
-
-
-  // headers: 
-  //  { host: 'localhost:8080',
-  //    connection: 'keep-alive',
-  //    pragma: 'no-cache',
-  //    'cache-control': 'no-cache',
-  //    accept: 'application/json, text/plain, */*',
-  //    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36',
-  //    referer: 'http://localhost:8080/',
-  //    'accept-encoding': 'gzip, deflate, sdch',
-  //    'accept-language': 'en-US,en;q=0.8' },
-
-   //  rawHeaders: 
-   // [ 'Host',
-   //   'localhost:8080',
-   //   'Connection',
-   //   'keep-alive',
-   //   'Pragma',
-   //   'no-cache',
-   //   'Cache-Control',
-   //   'no-cache',
-   //   'Accept',
-   //   'application/json, text/plain, */*',
-   //   'User-Agent',
-   //   'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36',
-   //   'Referer',
-   //   'http://localhost:8080/',
-   //   'Accept-Encoding',
-   //   'gzip, deflate, sdch',
-   //   'Accept-Language',
-   //   'en-US,en;q=0.8' ],
-
-  //  baseUrl: '/api/pokemon',
-  // originalUrl: '/api/pokemon',
